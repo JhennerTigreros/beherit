f5f52cf747d63abf3c740b56fa875411852b718d
Jhenner Tigreros <jtigreros16@gmail.com>
2024-01-31 21:47:18 -0500

    Change function types

--- beherit/utils.py
+++ beherit/utils.py
@@ -1,9 +1,10 @@
 import itertools
+from typing import Optional
 
 
-def check_dtype(data) -> str:
+def check_dtype(data) -> Optional[str]:
     dtype = set([type(x).__name__ for x in list(itertools.chain(*data))])
 
-    if len(dtype) > 1: return dtype
+    if len(dtype) > 1: return None
 
-    return dtype
+    return dtype[0]
